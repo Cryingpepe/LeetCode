e4c3765372c8ea7ac46e3c8209dd5079f983598d
Cryingpepe <82265937+Cryingpepe@users.noreply.github.com>
2024-12-31 17:30:00 +0900

    Time: 8 ms (94.41%), Space: 12.5 MB (10.32%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,12 +1,12 @@
 class Solution(object):
     def fib(self, n):
         
-        fibonacciList = [1, 1] 
+        fibonacciList = [0, 1, 1] 
 
-        for i in range(2, n+1):
+        for i in range(3, n+1):
             fibonacciList.append(fibonacciList[i-1] + fibonacciList[i-2])
     
-        return fibonacciList[n-1]
+        return fibonacciList[n]
     
 
         
