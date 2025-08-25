66e15d668d00b46aade6aa315ef7e41317fd9573
Seongbeen Na <82265937+Cryingpepe@users.noreply.github.com>
2025-08-24 20:15:20 -0400

    Time: 23 ms (36%), Space: 12.7 MB (89.92%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,23 +1,22 @@
 class Solution(object):
     def isPalindrome(self, s):
         start, end = 0, len(s) - 1
-
+        
         while start < end:
 
-            if not (48 <= ord(s[start]) <= 57 or 65 <= ord(s[start]) <= 90 or 97 <= ord(s[start]) <= 122):
+            if not s[start].isalnum():
                 start += 1
-                continue
 
-            if not (48 <= ord(s[end]) <= 57 or 65 <= ord(s[end]) <= 90 or 97 <= ord(s[end]) <= 122):
+            elif not s[end].isalnum():
                 end -= 1
-                continue
 
-            a, b = ord(s[start]), ord(s[end])
-
-            if a != b and not (65 <= a <= 90 and a + 32 == b) and not (97 <= a <= 122 and a - 32 == b):
-                return False
-
-            start += 1
-            end -= 1
+            else:
+                if s[start].lower() != s[end].lower():
+                    return False
+                    
+                start += 1
+                end -= 1
 
         return True
+
+        
