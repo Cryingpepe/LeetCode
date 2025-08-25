5d89a00fbd8a57d133492fb398300c9101cfe149
Seongbeen Na <82265937+Cryingpepe@users.noreply.github.com>
2025-08-25 10:07:02 -0400

    Time: 0 ms (100%), Space: 12.4 MB (70.45%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+
+        counter = 0
+        
+        if t == s:
+            return True
+        elif t == "":
+            return False
+
+        for i in s:
+            if counter == len(t):
+                return False
+            elif i == t[counter]:
+                counter += 1
+                continue
+            else:
+                while i != t[counter]:
+                    counter += 1
+                    if counter == len(t):
+                        return False
+                counter += 1
+        
+        return True
+            
+        
