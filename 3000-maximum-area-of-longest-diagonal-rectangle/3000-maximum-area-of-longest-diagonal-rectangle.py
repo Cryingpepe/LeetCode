eed160813e8468d12b3c34a87d4e1777726f1201
Seongbeen Na <82265937+Cryingpepe@users.noreply.github.com>
2025-08-26 08:58:12 -0400

    Time: 5 ms (16.95%), Space: 12.3 MB (98.31%) - LeetHub

---
+++ 3000-maximum-area-of-longest-diagonal-rectangle/3000-maximum-area-of-longest-diagonal-rectangle.py
@@ -0,0 +1,23 @@
+import math
+
+class Solution(object):
+    def areaOfMaxDiagonal(self, dimensions):
+        """
+        :type dimensions: List[List[int]]
+        :rtype: int
+        """
+        maximumDiagonal = 0
+        result = 0
+
+        for i in range(len(dimensions)):
+            x = dimensions[i][0]
+            y = dimensions[i][1]
+            z = math.sqrt(x * x + y * y)
+
+            if maximumDiagonal < z:
+                maximumDiagonal = z
+                result = x * y
+            elif z == maximumDiagonal:
+                result = max(result, x*y)
+        
+        return result
