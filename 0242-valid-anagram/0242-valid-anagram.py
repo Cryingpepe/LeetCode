d97f7cd926c2b496f12726dcc4efea1386a58dd4
Seongbeen Na <82265937+Cryingpepe@users.noreply.github.com>
2025-09-14 10:14:50 -0400

    Time: 60 ms (5.1%), Space: 12.5 MB (82.8%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
+        hashMap = {}
+
+        for i in s:
+            if i not in hashMap.keys():
+                hashMap[i] = 1
+            else:
+                hashMap[i] += 1
+
+        for i in t:
+            if i not in hashMap.keys():
+                return False
+            else:
+                hashMap[i] -= 1
+
+        for i in hashMap.values():
+            if i != 0:
+                return False
+        
+        return True
+        
