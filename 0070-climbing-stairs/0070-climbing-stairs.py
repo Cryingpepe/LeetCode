a8048654033a0f09c4d8bf9bb3c0e535ddfc6f0d
Seongbeen Na <82265937+Cryingpepe@users.noreply.github.com>
2025-09-16 09:08:44 -0400

    Time: 0 ms (100%), Space: 12.5 MB (12.99%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,12 +1,14 @@
 class Solution(object):
     def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
         
-        wayToNList = [1, 1] # base case: there is 1 way to go 0th stair and 1 way to go 1st stair 
+        stepsToN = [1, 1] # 1 way to 0th and 1st stairs
 
-        for i in range(2, n+1):
-            wayToNList.append(wayToNList[i-1] + wayToNList[i-2]) # since there are always two ways to get n (2 steps from n-2 or 1 step from n-1)
-    
-        return wayToNList[n]
-    
+        for i in range(2, n + 1):
+            stepsToN.append(stepsToN[i-2] + stepsToN[i-1])
 
-        
+        return stepsToN[n]
+
