3eeb9199e0e82680af54b942eb706b06a8aea8a6
Seongbeen Na <82265937+Cryingpepe@users.noreply.github.com>
2025-09-30 08:49:49 -0400

    Time: 152 ms (6.27%), Space: 37.8 MB (11.45%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -0,0 +1,45 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        
+        maxLength = 0
+        hashMap = {}
+
+        for i in nums:
+            if i not in hashMap and i - 1 not in hashMap and i + 1 not in hashMap:
+                hashMap[i] = 1
+
+                if hashMap[i] > maxLength:
+                    maxLength = hashMap[i]
+
+            if i not in hashMap and i - 1 in hashMap and i + 1 not in hashMap:
+                leftEndNum = i - hashMap[i - 1]
+
+                hashMap[i] = hashMap[leftEndNum] + 1
+                hashMap[leftEndNum] += 1
+
+                if hashMap[leftEndNum] > maxLength:
+                    maxLength = hashMap[leftEndNum]
+
+            if i not in hashMap and i - 1 not in hashMap and i + 1 in hashMap:
+                rightEndNum = i + hashMap[i + 1]
+
+                hashMap[i] = hashMap[rightEndNum] + 1
+                hashMap[rightEndNum] += 1
+
+                if hashMap[rightEndNum] > maxLength:
+                    maxLength = hashMap[rightEndNum]
+
+            if i not in hashMap and i - 1 in hashMap and i + 1 in hashMap:
+                rightEndNum = i + hashMap[i + 1]
+                leftEndNum = i - hashMap[i - 1]
+
+                temp = hashMap[rightEndNum]
+                hashMap[rightEndNum] += hashMap[leftEndNum] + 1
+                hashMap[leftEndNum] += temp + 1
+                hashMap[i] = hashMap[rightEndNum]
+
+                if hashMap[rightEndNum] > maxLength:
+                    maxLength = hashMap[rightEndNum]
+            
+        return maxLength
+            
