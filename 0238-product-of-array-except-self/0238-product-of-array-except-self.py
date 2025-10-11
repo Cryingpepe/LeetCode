0229aaaecf4cafc6b6853a1a190d2920c3f7a28d
Seongbeen Na <82265937+Cryingpepe@users.noreply.github.com>
2025-10-11 11:34:44 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -0,0 +1,24 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+        total = 0
+        result = []
+
+        for i in nums:
+            
+            if i != 0 and total != 0:
+                total *= i
+            elif i != 0 and total == 0:
+                total += i
+        
+        if 0 in nums:
+            for i in nums:
+                if i == 0:
+                    result.append(total)
+                else:
+                    result.append(0)
+        else:
+            for i in nums:
+                result.append(total // i)
+        
+        return result
