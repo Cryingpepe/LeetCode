10fa6c4ef4cab607e57ac8245436b90036feb532
Seongbeen Na <82265937+Cryingpepe@users.noreply.github.com>
2025-10-13 10:54:10 -0400

    Time: 20 ms (74.26%), Space: 23.2 MB (80.23%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -28,3 +28,18 @@ class Solution:
                 result.append(total // i)
         
         return result
+
+        # n = len(nums)
+        # result = [1] * n
+
+        # prefix = 1
+        # for i in range(n):
+        #     result[i] = prefix
+        #     prefix *= nums[i]
+
+        # postfix = 1
+        # for i in range(n - 1, -1, -1):
+        #     result[i] *= postfix
+        #     postfix *= nums[i]
+
+        # return result
