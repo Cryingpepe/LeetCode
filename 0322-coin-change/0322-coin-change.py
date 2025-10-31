c3efb836699a98739ac1a43b85bb7fb387009f92
Seongbeen Na <82265937+Cryingpepe@users.noreply.github.com>
2025-10-31 11:10:04 -0400

    Time: 780 ms (49%), Space: 18.3 MB (57.41%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -6,7 +6,7 @@ class Solution:
 
         for i in range(1, amount + 1):
             for coin in coins:
-                if amount - coin > 0:
+                if amount - coin >= 0:
                     dp[i] = min(dp[i], dp[i - coin] + 1)
         
         return dp[amount] if dp[amount] < amount + 1 else -1
