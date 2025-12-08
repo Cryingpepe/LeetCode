ae1e0027c41a34816afd4b0b38204a0089e1da50
Seongbeen Na <82265937+Cryingpepe@users.noreply.github.com>
2025-12-08 11:30:41 -0500

    Time: 0 ms (100%), Space: 18 MB (11.68%) - LeetHub

---
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countTriples(self, n: int) -> int:
+        
+        result = 0
+        
+        for u in range(2, int(sqrt(n)) + 1):
+            for v in range(1, u):
+                if (u - v) & 1 == 0 or gcd(u, v) != 1:
+                    continue
+ 
+                c = u * u + v * v
+
+                if c > n:
+                    continue
+
+                result += 2 * (n // c)
+
+        return result
