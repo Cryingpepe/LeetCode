81df634e49cf5858599ad0996f913549d501c232
Seongbeen Na <82265937+Cryingpepe@users.noreply.github.com>
2026-01-01 10:41:16 -0500

    Time: 0 ms (100%), Space: 17.1 MB (99.34%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,38 +1,12 @@
-class Solution(object):
-    def plusOne(self, digits):
-        """
-        :type digits: List[int]
-        :rtype: List[int]
-        """
-
-        isRoundedUp = False
-
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        
         for i in range(len(digits) - 1, -1, -1):
+            
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
 
-            if not isRoundedUp:
+            digits[i] = 0
 
-                if digits[i] == 9:
-                    digits[i] = 0
-                    isRoundedUp = True
-                else:
-                    digits[i] = digits[i] + 1
-                    break
-            else:
-                if digits[i] == 9:
-                    digits[i] = 0
-                    isRoundedUp = True
-                else:
-                    digits[i] = digits[i] + 1
-                    isRoundedUp = False
-                    break
-        
-        if isRoundedUp:
-            return [1] + digits
-        else:
-            return digits
-
-
-
-
-
-        
+        return [1] + digits
