b489875f6ebd606df9ba5a4a36c78edb7bdadb99
Seongbeen Na <82265937+Cryingpepe@users.noreply.github.com>
2026-01-18 11:10:02 -0500

    Time: 1748 ms (5.06%), Space: 26.5 MB (20.69%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,12 +1,9 @@
-class Solution(object):
-    def rotate(self, nums, k):
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
         """
-        :type nums: List[int]
-        :type k: int
-        :rtype: None Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in-place instead.
         """
-
-        n = len(nums)
-        k = k % n
         
-        nums[:] = nums[-k:] + nums[:-k]
+        for i in range(k):
+            right = nums.pop()
+            nums.insert(0, right)
