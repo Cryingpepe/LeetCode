6bfdc289d4554097396af8db2de38dfdf379ef87
Seongbeen Na <82265937+Cryingpepe@users.noreply.github.com>
2026-02-14 10:50:47 -0500

    Time: 81 ms (59.9%), Space: 19.7 MB (20.76%) - LeetHub

---
+++ 0799-champagne-tower/0799-champagne-tower.py
@@ -0,0 +1,18 @@
+class Solution:
+    def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
+        
+        dp = [[0.0] * (query_row + 2) for _ in range(query_row + 1)]
+        dp[0][0] = float(poured)
+
+        for r in range(query_row):
+
+            for c in range(r + 1):
+
+                overflow = max(0.0, dp[r][c] - 1.0)
+
+                if overflow > 0:
+                    leftOver = overflow / 2.0
+                    dp[r + 1][c] += leftOver
+                    dp[r + 1][c + 1] += leftOver
+
+        return min(1.0, dp[query_row][query_glass])
