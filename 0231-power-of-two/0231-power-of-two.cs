3d3a8ae67267f0b34de89e526564f65be6142471
Seongbeen Na <82265937+Cryingpepe@users.noreply.github.com>
2026-04-25 10:23:17 -0400

    Time: 0 ms (100%), Space: 30.9 MB (36.74%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public bool IsPowerOfTwo(int n) {
+        
+        if(n == 0)
+        {
+            return false;
+        }
+        
+        while(n != 1)
+        {
+           if(n % 2 == 0)
+                n/=2;
+            else 
+                break;
+        }
+
+        return n == 1;
+    }
+}
